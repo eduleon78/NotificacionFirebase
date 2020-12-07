5f03300e11291e72395c1354c5fc6a63bc3a7d10
eduleon78 <66341077+eduleon78@users.noreply.github.com>
2020-12-06 20:50:19 -0400

    Path /Android
    Signed-off-by: eduleon78 <66341077+eduleon78@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express = require('express')
-const path = require('path')
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
 const PORT = process.env.PORT || 5000
 
-var bodyParser = require("body-parser")
-  .use(bodyParser.json())  //soporte para codificar json
-  .use(bodyParser.urlencoded({ extended: true}))  //soporte para codificar las url
+// Configurar express
+
+var app = express();
+
+var router = express.Router();
+
+app.use(bodyParser.json());  //soporte para codificar json
+app.use(bodyParser.urlencoded({ extended: true}));  //soporte para codificar las url
+
+var firebase = require("firebase");
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -12,12 +21,12 @@ express()
   .set('view engine', 'ejs')
 
 
-  .get('/android', (req, res) => res.render('pages/index'))
+  .get('/android', function (req, res) => res.render('pages/index'))
 
   //POST
   //https://desolate-harbor-16746.herokuapp.com/token-device
   //token
-  .post("/token-device", (req, res) => res.send(request.body.token))
+  app.post("/token-device", function (req, res) => res.send(request.body.token));
 
 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
